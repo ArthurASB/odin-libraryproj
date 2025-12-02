45fa3149eb6c23b0f16f8c6a4a5c9d527827e48c
ArthurASB <195683488+ArthurASB@users.noreply.github.com>
2025-12-02 16:45:28 -0300

    Factor basic addition and display functions

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 
 const myLibrary = [];
 
-function Book(title, author, read, ID) {
+function Book(title, author, read, pages, ID) {
     if (!new.target) {
         throw new Error("You must use the 'new' operator to call the constructor");
     }
 	this.title = title;
 	this.author = author;
 	this.read = read;
+	this.pages = pages;
 	this.ID = ID;
 }
 
-function addBooktoLibrary (bktitle, bkauthor, bkstatus) {
-    const bookID = crypto.randomUUID ();
-
-
-	myLibrary.push();
+function addBooktoLibrary (bktitle, bkauthor, bkstatus, bkpages) {
+    const bookID = crypto.randomUUID();
+	const newBook = new Book (bktitle, bkauthor, bkstatus, bkpages, bookID);
+	myLibrary.push(newBook);
 }
+
+function displayLibrary () {
+	for (const book of myLibrary) {
+		console.log(book);
+	}
+}
+
+addBooktoLibrary ("The Stranger", "Albert Camus", "read", 240);
+displayLibrary();
